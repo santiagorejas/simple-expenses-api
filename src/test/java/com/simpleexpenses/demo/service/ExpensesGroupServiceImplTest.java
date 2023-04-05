9b41250cf06694ce253e3e6baf05481be69717ca
santiagorejas <santiago.orejasrg@gmail.com>
2023-04-05 19:15:07 -0300

    update expenses group service test

--- src/test/java/com/simpleexpenses/demo/service/ExpensesGroupServiceImplTest.java
+++ src/test/java/com/simpleexpenses/demo/service/ExpensesGroupServiceImplTest.java
@@ -1,23 +1,20 @@
 package com.simpleexpenses.demo.service;
 
 import com.simpleexpenses.demo.dto.ExpensesGroupDto;
+import com.simpleexpenses.demo.exceptions.AccessDeniedException;
+import com.simpleexpenses.demo.exceptions.EntityNotFoundException;
 import com.simpleexpenses.demo.model.entity.ExpenseEntity;
 import com.simpleexpenses.demo.model.entity.ExpensesGroupEntity;
 import com.simpleexpenses.demo.repository.ExpenseRepository;
 import com.simpleexpenses.demo.repository.ExpensesGroupRepository;
 import com.simpleexpenses.demo.service.impl.ExpensesGroupServiceImpl;
-import org.checkerframework.checker.nullness.Opt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -26,6 +23,7 @@ import java.math.BigDecimal;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -42,11 +40,28 @@ public class ExpensesGroupServiceImplTest {
     @Mock
     ExpenseRepository expenseRepository;
 
+    private ExpensesGroupEntity expensesGroupEntity;
+
+    private final String title = "Expenses group test.";
+    private final String description = "Description.";
+    private final String userId = "userId";
+
+    @BeforeEach
+    void setUp() {
+        Jwt jwt = Mockito.mock(Jwt.class);
+        when(jwt.getSubject()).thenReturn(userId);
+        SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(jwt));
+
+        this.expensesGroupEntity = ExpensesGroupEntity
+                .builder()
+                .userId(userId)
+                .title(title)
+                .description(description)
+                .build();
+    }
+
     @Test
     final void testCreateExpenseGroup() {
-
-        String title = "Expenses group test.";
-        String description = "Description.";
 
         ExpensesGroupDto expensesGroupDto = ExpensesGroupDto
                 .builder()
@@ -54,17 +69,7 @@ public class ExpensesGroupServiceImplTest {
                 .description(description)
                 .build();
 
-        ExpensesGroupEntity expensesGroupEntity = ExpensesGroupEntity
-                .builder()
-                .title(title)
-                .description(description)
-                .build();
-
-        Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.getSubject()).thenReturn("userid");
-        SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(jwt));
-
-        when(this.expensesGroupRepository.save(any(ExpensesGroupEntity.class))).thenReturn(expensesGroupEntity);
+        when(this.expensesGroupRepository.save(any(ExpensesGroupEntity.class))).thenReturn(this.expensesGroupEntity);
 
         ExpensesGroupDto createdExpensesGroup =
                 this.expensesGroupService.createExpensesGroup(expensesGroupDto);
@@ -100,13 +105,11 @@ public class ExpensesGroupServiceImplTest {
         expensesGroupEntities.add(exp2);
         expensesGroupEntities.add(exp3);
 
-        Jwt jwt = Mockito.mock(Jwt.class);
-        when(jwt.getSubject()).thenReturn(userId);
-        SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(jwt));
-
         when(this.expensesGroupRepository.findAllByUserId(userId)).thenReturn(Optional.of(expensesGroupEntities));
 
         List<ExpensesGroupDto> expensesGroupDtoList = this.expensesGroupService.getExpensesGroups();
+
+        assertEquals(expensesGroupDtoList.size(), expensesGroupEntities.size());
 
         ExpensesGroupDto currentDto;
         ExpensesGroupEntity currentEntity;
@@ -123,9 +126,86 @@ public class ExpensesGroupServiceImplTest {
                 assertEquals(currentDto.getExpenses().size(), currentEntity.getExpenses().size());
             }
         }
+    }
 
+    @Test
+    final void testUpdateExpensesGroup() {
 
+        String newTitle = "This is the new title.";
+        String newDescription = "This is the new Description.";
+        String expensesGroupId = UUID.randomUUID().toString();
 
+        ExpensesGroupDto expensesGroupDto = ExpensesGroupDto
+                .builder()
+                .expensesGroupId(expensesGroupId)
+                .title(newTitle)
+                .description(newDescription)
+                .userId(userId)
+                .build();
+
+        ExpensesGroupEntity updatedExpensesGroupEntity = ExpensesGroupEntity
+                .builder()
+                .expensesGroupId(expensesGroupId)
+                .title(newTitle)
+                .description(newDescription)
+                .userId(userId)
+                .build();
+
+        when(this.expensesGroupRepository.findByExpensesGroupId(anyString())).thenReturn(Optional.of(this.expensesGroupEntity));
+        when(this.expensesGroupRepository.save(any(ExpensesGroupEntity.class))).thenReturn(updatedExpensesGroupEntity);
+
+        ExpensesGroupDto updatedExpensesGroupDto = this.expensesGroupService.updateExpensesGroup("someId", expensesGroupDto);
+
+        assertEquals(updatedExpensesGroupDto.getTitle(), expensesGroupDto.getTitle());
+        assertEquals(updatedExpensesGroupDto.getDescription(), expensesGroupDto.getDescription());
+        assertEquals(updatedExpensesGroupDto.getUserId(), expensesGroupDto.getUserId());
+        assertEquals(updatedExpensesGroupDto.getExpensesGroupId(), expensesGroupDto.getExpensesGroupId());
+
+    }
+
+    @Test
+    final void testUpdateExpensesGroup_EntityNotFoundException() {
+
+        String newTitle = "This is the new title.";
+        String newDescription = "This is the new Description.";
+
+        ExpensesGroupDto expensesGroupDto = ExpensesGroupDto
+                .builder()
+                .title(newTitle)
+                .description(newDescription)
+                .build();
+
+        when(this.expensesGroupRepository.findByExpensesGroupId(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            this.expensesGroupService.updateExpensesGroup("someId", expensesGroupDto);
+        });
+    }
+
+    @Test
+    final void testUpdateExpensesGroup_AccessDeniedException() {
+
+        String newTitle = "This is the new title.";
+        String newDescription = "This is the new Description.";
+
+        ExpensesGroupDto expensesGroupDto = ExpensesGroupDto
+                .builder()
+                .title(newTitle)
+                .description(newDescription)
+                .build();
+
+        ExpensesGroupEntity expensesGroup = ExpensesGroupEntity
+                .builder()
+                .title(title)
+                .description(description)
+                .userId("anotherUserId")
+                .build();
+
+        when(this.expensesGroupRepository.findByExpensesGroupId(anyString())).thenReturn(Optional.of(expensesGroup));
+
+        assertThrows(AccessDeniedException.class, () -> {
+            this.expensesGroupService.updateExpensesGroup("someId", expensesGroupDto);
+        });
     }
 
 }
