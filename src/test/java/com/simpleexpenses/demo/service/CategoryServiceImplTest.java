94db97ce10c4fb86e3423dfaba2a7f1f2db17cfe
santiagorejas <santiago.orejasrg@gmail.com>
2023-04-07 20:52:10 -0300

    get categories service test

--- src/test/java/com/simpleexpenses/demo/service/CategoryServiceImplTest.java
+++ src/test/java/com/simpleexpenses/demo/service/CategoryServiceImplTest.java
@@ -16,6 +16,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -72,4 +77,45 @@ public class CategoryServiceImplTest {
         assertEquals(userId, createdCategoryDto.getUserId());
     }
 
+    private List<CategoryEntity> buildCategories() {
+        CategoryEntity c1 = CategoryEntity.builder().Id(1L).categoryId("id1").title("t1").color("#ccc").userId(userId).build();
+        CategoryEntity c2 = CategoryEntity.builder().Id(2L).categoryId("id2").title("t2").color("#ccc").userId(userId).build();
+        CategoryEntity c3 = CategoryEntity.builder().Id(3L).categoryId("id3").title("t3").color("#ccc").userId(userId).build();
+
+        List<CategoryEntity> categories = new ArrayList<>();
+        categories.add(c1);
+        categories.add(c2);
+        categories.add(c3);
+
+        return categories;
+    }
+
+    @Test
+    final void testGetCategories() {
+
+        List<CategoryEntity> categories = this.buildCategories();
+
+        when(this.categoryRepository.findAllByUserId(userId)).thenReturn(Optional.of(categories));
+
+        List<CategoryDto> categoriesDtoList = this.categoryService.getCategories();
+
+        Iterator<CategoryEntity> entityIterator = categories.iterator();
+        Iterator<CategoryDto> dtoIterator = categoriesDtoList.iterator();
+
+        assertEquals(categories.size(), categoriesDtoList.size());
+
+        CategoryEntity currentCategoryEntity;
+        CategoryDto currentCategoryDto;
+
+        while (entityIterator.hasNext() && dtoIterator.hasNext()) {
+            currentCategoryEntity = entityIterator.next();
+            currentCategoryDto = dtoIterator.next();
+
+            assertEquals(currentCategoryEntity.getId(), currentCategoryDto.getId());
+            assertEquals(currentCategoryEntity.getCategoryId(), currentCategoryDto.getCategoryId());
+            assertEquals(currentCategoryEntity.getTitle(), currentCategoryDto.getTitle());
+            assertEquals(currentCategoryEntity.getColor(), currentCategoryDto.getColor());
+            assertEquals(currentCategoryEntity.getUserId(), currentCategoryDto.getUserId());
+        }
+    }
 }
